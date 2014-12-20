671aede918c4fb6ceca94da32e500f6ed12c18f1
Jonathan <jonathan.t.barnard@gmail.com>
2014-12-21 00:39:54 +1100

    Sped up java

--- jv.java
+++ jv.java
@@ -1,78 +1,79 @@
-import java.util.ArrayList;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
-class refInt{
-    public int val;
-}
-
-class longestPathFinder{
-   ArrayList<node> readPlaces(refInt numNodes) throws Exception{
-	BufferedReader in = new BufferedReader(new FileReader("agraph"));
-	String s = in.readLine();
-	numNodes.val = Integer.parseInt(s);
-	ArrayList<node> nodes = new ArrayList<>(numNodes.val);
-	for(int i = 0; i < numNodes.val; i++){
-	    node n = new node();
-	    n.neighbours = new ArrayList<>();
-	    nodes.add(n);
-	}
-	while (in.ready()) {
-	    String ln = in.readLine();
-	    String[] nums = ln.split("[ \t]+");
-	    if(nums.length != 3){
-		break;
-	    }
-	    int node = Integer.parseInt(nums[0]);
-	    int neighbour = Integer.parseInt(nums[1]);
-	    int cost = Integer.parseInt(nums[2]);
-	    nodes.get(node).neighbours.add(new route(neighbour, cost));
-	}
-	in.close();	
-	return nodes;
-    }
-
-    int getLongestPath(ArrayList<node> nodes, int nodeID, boolean[] visited){
-	visited[nodeID] = true;
-	int dist, max=0;
-	for(route neighbour: nodes.get(nodeID).neighbours){
-	    if (!visited[neighbour.dest]){
-		dist = neighbour.cost + getLongestPath(nodes, neighbour.dest, visited);
-		if (dist > max){
-		    max = dist;
-		}
-	    }    
-	}
-	visited[nodeID] = false;
-	return max;
-    }
-    
-}
-
-class route{
-	int dest, cost;
-	route(int dest, int cost){
-	    this.dest =dest;
-	    this.cost =cost;
-	}
-    }
-
-class node {
-    ArrayList<route> neighbours;
-}
+import java.util.Arrays;
 
 public class jv{
-    public static void main(String[] args) throws Exception{
-	refInt numNodes = new refInt();
-	longestPathFinder p = new longestPathFinder();
-        ArrayList<node> nodes = p.readPlaces(numNodes);
-	boolean[] visited = new boolean[numNodes.val];
-	for(boolean b: visited){
-	    b = false;
+
+	static int numNodes = -1;
+	static final int[][] nodes;
+	static final boolean[] visited;
+
+	static {
+		nodes = readPlaces();
+		visited = new boolean[numNodes];
 	}
-	long start = System.currentTimeMillis();
-        int len = p.getLongestPath(nodes, 0, visited);
-	long duration = System.currentTimeMillis() - start;
-	System.out.printf("%d LANGUAGE Java %d\n", len, duration);	
-    }
+
+	public static void main(final String[] args) throws Exception{
+		final long start = System.currentTimeMillis();
+		final int len = getLongestPath(0);
+		final long duration = System.currentTimeMillis() - start;
+		System.out.printf("%d LANGUAGE Java %d\n", len, duration);
+	}
+
+	/**
+	 * int[node][dest|cost|...]
+	 */
+	static int[][] readPlaces() {
+		try (BufferedReader in = new BufferedReader(new FileReader("agraph"))) {
+			final String s = in.readLine();
+			numNodes = Integer.parseInt(s);
+			final int[][] nodes = new int[numNodes][];
+			for(int i = 0; i < numNodes; i++){
+				nodes[i]= new int[0];
+			}
+			while (in.ready()) {
+				final String ln = in.readLine();
+				final String[] nums = ln.split("[ \t]+");
+				if(nums.length != 3){
+					break;
+				}
+				final int node = Integer.parseInt(nums[0]);
+				final int neighbour = Integer.parseInt(nums[1]);
+				final int cost = Integer.parseInt(nums[2]);
+
+				final int index = nodes[node].length;
+				final int[] replacement = Arrays.copyOf(nodes[node], index + 2);
+				replacement[index] = neighbour;
+				replacement[index+1] = cost;
+
+				nodes[node] = replacement;
+			}
+			return nodes;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+	static int getLongestPath(final int nodeID){
+		visited[nodeID] = true;
+		int dist, max=0;
+
+		final int length = nodes[nodeID].length;
+
+		for (int i = 0; i < length; i+=2) {
+
+			final int dest = nodes[nodeID][i];
+
+			if (!visited[dest]) {
+				dist = nodes[nodeID][i + 1] + getLongestPath(dest);
+				if (dist > max) {
+					max = dist;
+				}
+			}
+		}
+
+		visited[nodeID] = false;
+		return max;
+	}
+
 }

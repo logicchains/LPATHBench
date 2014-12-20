e72cf67c3fb43e52dff2781444dec4741e28be70
lemmi <lemmi@nerd2nerd.org>
2014-12-20 13:35:10 +0100

    go.go: go fmt

--- go.go
+++ go.go
@@ -1,15 +1,15 @@
 package main
 
-import(
+import (
+	"fmt"
 	"io/ioutil"
 	"strconv"
 	"strings"
 	"time"
-	"fmt"
 )
 
-type route struct{
-	to int32
+type route struct {
+	to   int32
 	cost int32
 }
 
@@ -17,54 +17,54 @@ type node struct {
 	neighbours []route
 }
 
-func readPlaces()([]node, int){
+func readPlaces() ([]node, int) {
 	bytes, err := ioutil.ReadFile("agraph")
 	if err != nil {
 		panic(err)
 	}
-	lines := strings.Split(string(bytes),"\n")
+	lines := strings.Split(string(bytes), "\n")
 	numNodes, err := strconv.Atoi(lines[0])
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	lines = lines[1:]
 	nodes := make([]node, numNodes)
-	for i := range nodes{
+	for i := range nodes {
 		nodes[i].neighbours = make([]route, 0, numNodes/2)
 	}
-	for _,ln := range lines{
-		nums := strings.Split(ln," ")
-		if len(nums) < 3{
+	for _, ln := range lines {
+		nums := strings.Split(ln, " ")
+		if len(nums) < 3 {
 			break
 		}
 		node, err1 := strconv.Atoi(nums[0])
 		neighbour, err2 := strconv.Atoi(nums[1])
 		cost, err3 := strconv.Atoi(nums[2])
-		if err1 != nil || err2 != nil || err3 != nil{
+		if err1 != nil || err2 != nil || err3 != nil {
 			panic("Error: encountered a line that wasn't three integers")
 		}
-		nodes[node].neighbours = append(nodes[node].neighbours, route{to:int32(neighbour), cost:int32(cost)})
+		nodes[node].neighbours = append(nodes[node].neighbours, route{to: int32(neighbour), cost: int32(cost)})
 	}
-        return nodes, numNodes
+	return nodes, numNodes
 }
 
-func getLongestPath(nodes []node, nodeID int32, visited []bool) int32{
+func getLongestPath(nodes []node, nodeID int32, visited []bool) int32 {
 	visited[nodeID] = true
 	var max int32
-	for _, neighbour := range nodes[nodeID].neighbours{
-		if !visited[neighbour.to]{
+	for _, neighbour := range nodes[nodeID].neighbours {
+		if !visited[neighbour.to] {
 			dist := neighbour.cost + getLongestPath(nodes, neighbour.to, visited)
-			if dist > max{
+			if dist > max {
 				max = dist
 			}
 		}
-		
+
 	}
 	visited[nodeID] = false
 	return max
 }
 
-func main(){
+func main() {
 	nodes, nNodes := readPlaces()
 	visited := make([]bool, nNodes)
 	start := time.Now()

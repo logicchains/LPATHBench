6a80f5405921130647ee7794ff8462d88b9b3ae7
James Preiss <jamesalanpreiss@gmail.com>
2014-12-21 13:03:25 -0500

    fix autoindents

--- cpp.cpp
+++ cpp.cpp
@@ -27,23 +27,23 @@ std::pair<vector<node>, vector<route>> readPlaces(){
   int totalNeighbours = 0;
   while (text >> node >> neighbour >> cost){
     nodes[node].nCount += 1;
-	totalNeighbours += 1;
+    totalNeighbours += 1;
   }
   // second pass: compute each node's offset in allNeighbours
   std::vector<route> allNeighbours(totalNeighbours);
   int cumulative = 0;
   for (auto &node : nodes) {
     node.nBegin = cumulative;
-	cumulative += node.nCount;
-	node.nCount = 0;
+    cumulative += node.nCount;
+    node.nCount = 0;
   }
   // fill in neighbours arrays
   text = ifstream("agraph");
   text >> numNodes;
   while (text >> node >> neighbour >> cost){
     auto &n = nodes[node];
-	allNeighbours[n.nBegin + n.nCount] = { neighbour, cost };
-	++n.nCount;
+    allNeighbours[n.nBegin + n.nCount] = { neighbour, cost };
+    ++n.nCount;
   }
   return std::make_pair(std::move(nodes), std::move(allNeighbours));
 }
@@ -57,7 +57,7 @@ int getLongestPath(const vector<node> &nodes, const vector<route> &neighbours, c
     const route &neighbour = neighbours[n.nBegin + i];
     if (visited[neighbour.dest] == false){
       const int dist = neighbour.cost + getLongestPath<T>(nodes, neighbours, neighbour.dest, visited);
-	  longest = max(longest, dist);
+      longest = max(longest, dist);
     }
   }
   visited[nodeID] = false;

faa1eca98fb827bf5b7841f9b893610ef0ec462b
Robert Åkerblom-Andersson <Robert@dartvoid.com>
2014-12-21 03:32:03 +0100

    Cleanup of Dart code and improved performance

--- dart.dart
+++ dart.dart
@@ -2,59 +2,59 @@ import 'dart:io';
 
 class Route {
   final int dest, cost;
+
   Route(this.dest, this.cost);
 
-  function toString(){
-    print("$dest, $cost\n");
-  }
+  String toString() => "$dest $cost\n";
 }
 
 class Node{
-  List<Route> neighbours;
-  Node(){
-    this.neighbours = new List<Route>();
+  final List<Route> neighbours = new List<Route>();
+
+  Node();
+}
+
+void readPlacesAndFindPath() {
+  List<String> lines = new File('agraph').readAsLinesSync();
+  int numNodes = int.parse(lines[0]);
+  List nodes = new List<Node>.generate(numNodes, (int index) => new Node());
+
+  for(int i = 1; i < lines.length; i++){
+    List nums = lines[i].split(' ');
+    int node = int.parse(nums[0]);
+    int neighbour = int.parse(nums[1]);
+    int cost = int.parse(nums[2]);
+
+    nodes[node].neighbours.add(new Route(neighbour,cost));
   }
+
+  var visited = new List<bool>.generate(numNodes, (int index) => false);
+  var start = new DateTime.now();
+  int len = getLongestPath(nodes, 0, visited);
+  var duration = new DateTime.now().difference(start);
+
+  print("$len LANGUAGE Dart ${duration.inMilliseconds}");
 }
 
-readPlacesAndFindPath() {
-  var nodes;
-  new File('agraph').readAsLines().then((List<String> lines) {
-    final int numNodes = int.parse(lines[0]);
-    final nodes = new List<Node>.generate(numNodes, (int index) => new Node()); 
-    for(int i = 1; i < lines.length; i++){
-      final nums = lines[i].split(' ');
-      int node = int.parse(nums[0]);
-      int neighbour = int.parse(nums[1]);      
-      int cost = int.parse(nums[2]);
-      nodes[node].neighbours.add(new Route(neighbour,cost));
-    }
-    var visited = new List<Bool>.generate(numNodes, (int index) => false);
-    var start = new DateTime.now();
-    int len = getLongestPath(nodes, 0, visited);
-    var duration = new DateTime.now().difference(start);
-    duration = duration.inMilliseconds;
-    print("$len LANGUAGE Dart $duration");
-  });
-  return nodes;
-}
-
-getLongestPath(List<Node> nodes, int nodeID, List<Bool> visited){
+int getLongestPath(List<Node> nodes, int nodeID, List<bool> visited){
   visited[nodeID] = true;
-  int max=0;
-  nodes[nodeID].neighbours.forEach((Route neighbour) {
-    if (!visited[neighbour.dest]){
-      final int dist = neighbour.cost + getLongestPath(nodes, neighbour.dest, visited);
+  int max = 0;
+
+  for(var i = 0; i < nodes[nodeID].neighbours.length; i++) {
+    if (!visited[nodes[nodeID].neighbours[i].dest]) {
+      final int dist = nodes[nodeID].neighbours[i].cost + getLongestPath(nodes, nodes[nodeID].neighbours[i].dest, visited);
       if (dist > max){
-	max = dist;
+        max = dist;
       }
     }
-  });    
+  }
+
   visited[nodeID] = false;
+
   return max;
 }
 
-
-
-main() {
+void main() {
   readPlacesAndFindPath();
 }
+

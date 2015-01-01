cb906fccfc45307b28da532dca859e9703df4c47
jonathan <jonathan.t..barnard@gmail.com>
2015-01-01 16:44:08 +1100

    REplaced C# with safe version that performs just as well

--- cs.cs
+++ cs.cs
@@ -3,105 +3,95 @@ using System.Collections.Generic;
 
 class longestPathFinder
 {
-	public node[] ReadPlaces(string path)
-	{
-		string[] lines = System.IO.File.ReadAllLines(path);
-		int numNodes = Int32.Parse(lines[0]);
+  
+  public node[] ReadPlaces(string path)
+  {
+    string[] lines = System.IO.File.ReadAllLines(path);
+    int numNodes = Int32.Parse(lines[0]);
+    node[] nodes = new node[numNodes];
+    List<route>[] routes = new List<route>[numNodes];
+    for (int i = 0; i < numNodes; i++)
+    {
+      nodes[i] = new node();
+      routes[i] = new List<route>();
+    }
+    for (int i = 1; i < lines.Length; i++)
+    {
+      string[] nums = lines[i].Split(' ');
+      if (nums.Length < 3)
+      {
+        break;
+      }
+      int node = Int32.Parse(nums[0]);
+      int neighbour = Int32.Parse(nums[1]);
+      int cost = Int32.Parse(nums[2]);
+      routes[node].Add(new route(neighbour, cost));
+    }
+    for (int i = 0; i < routes.Length; i++)
+    {
+      nodes[i].neighbours = routes[i].ToArray();
+    }
+    return nodes;
+  }
 
-		node[] nodes = new node[numNodes];
-		List<route>[] routes = new List<route>[numNodes];
-		for (int i = 0; i < numNodes; i++)
-		{
-			nodes[i] = new node();
-			routes[i] = new List<route>();
-		}
+  public int getLongestPath(node[] nodes, int nodeIndex, bool[] visited)
+  {
+    
 
-		for (int i = 1; i < lines.Length; i++)
-		{
-			string[] nums = lines[i].Split(' ');
-			if (nums.Length < 3)
-			{
-				break;
-			}
-			int node = Int32.Parse(nums[0]);
-			int neighbour = Int32.Parse(nums[1]);
-			int cost = Int32.Parse(nums[2]);
-			routes[node].Add(new route(neighbour, cost));
-		}
+    int max = 0;
+    visited[nodeIndex] = true;
+    route[] neighbours = nodes[nodeIndex].neighbours;
 
-		for (int i = 0; i < routes.Length; i++)
-		{
-			nodes[i].neighbours = routes[i].ToArray();
-		}
 
-		return nodes;
-	}
+    for (var i = 0; i < neighbours.Length; i++)
+    {
+      if (!visited[neighbours[i].dest])
+      {
+        int dist = neighbours[i].cost + getLongestPath(nodes, neighbours[i].dest, visited);
+        if (dist > max)
+        {
+          max = dist;
+        }
+      }
+    }
 
-	public unsafe int getLongestPath(node[] nodes, int nodeIndex, bool[] visited)
-	{
-		fixed (bool* pVisited = visited)
-		{
-			return getLongestPath(nodes, 0, pVisited);
-		}
-	}
+    visited[nodeIndex] = false;
 
-	[System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.AggressiveInlining)]
-	private unsafe int getLongestPath(node[] nodes, int nodeIndex, bool* visited)
-	{
-		int max = 0;
-		visited[nodeIndex] = true;
-
-		route[] neighbours = nodes[nodeIndex].neighbours;
-		fixed (route* pNeighbours = neighbours)
-		{
-			route* pLength = pNeighbours + neighbours.Length;
-			for (route* neighbour = pNeighbours; neighbour < pLength; neighbour++)
-			{
-				if (!visited[neighbour->dest])
-				{
-					int dist = neighbour->cost + getLongestPath(nodes, neighbour->dest, visited);
-					if (dist > max)
-					{
-						max = dist;
-					}
-				}
-			}
-		}
-
-		visited[nodeIndex] = false;
-		return max;
-	}
+    return max;
+  }
 }
-
 struct route
 {
-	public int dest, cost;
-	public route(int dest, int cost)
-	{
-		this.dest = dest;
-		this.cost = cost;
-	}
+  public int dest, cost;
+  public route(int dest, int cost)
+  {
+    this.dest = dest;
+    this.cost = cost;
+  }
 }
+
 
 struct node
 {
-	public route[] neighbours;
+  public route[] neighbours;
 }
-
 class cs
 {
-	static void Main(string[] args)
-	{
-		longestPathFinder p = new longestPathFinder();
-		node[] nodes = p.ReadPlaces("agraph");
-		bool[] visited = new bool[nodes.Length];
 
-		System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
-		timer.Start();
 
-		int result = p.getLongestPath(nodes, 0, visited);
 
-		timer.Stop();
-		Console.WriteLine("{0} LANGUAGE CSharp {1} ms", result, Math.Round(timer.Elapsed.TotalMilliseconds));
-	}
+  static void Main(string[] args)
+  {
+
+
+
+    longestPathFinder p = new longestPathFinder();
+    node[] nodes = p.ReadPlaces("agraph");
+    bool[] visited = new bool[nodes.Length];
+    System.Diagnostics.Stopwatch timer = new System.Diagnostics.Stopwatch();
+    timer.Start();
+    int result = p.getLongestPath(nodes, 0, visited);
+    timer.Stop();
+    Console.WriteLine("{0} LANGUAGE CSharp {1} ms", result, Math.Round(timer.Elapsed.TotalMilliseconds));
+  }
 }
